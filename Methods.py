7bb6a1717dda4603fbfdc9bea08953b9aa6c6fb6
George Lewis <sgt.george.lewis@gmail.com>
2022-07-20 14:38:16 -0400

    start of my python scripts

---
+++ Methods.py
@@ -0,0 +1,24 @@
+Python 3.10.5 (tags/v3.10.5:f377153, Jun  6 2022, 16:14:13) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+x = "I love this"
+x = x + " with you"
+x
+'I love this with you'
+x = "Hello"
+x
+'Hello'
+"hello" + "World"
+'helloWorld'
+"hello" + " World"
+'hello World'
+x.upper()
+'HELLO'
+x
+'Hello'
+x.
+SyntaxError: invalid syntax
+#1. Are strings mutable?
+#Strings are not mutable! (meaning you can't use indexing to change individual e#lements of a string)
+
+#2. How do I create comments in my code?
+#You can use the hashtag # to create comments in your code
---
+++ String Indexing.py
@@ -0,0 +1,34 @@
+Python 3.10.5 (tags/v3.10.5:f377153, Jun  6 2022, 16:14:13) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+mystring + adadfsasdf
+Traceback (most recent call last):
+  File "<pyshell#0>", line 1, in <module>
+    mystring + adadfsasdf
+NameError: name 'mystring' is not defined
+mystring = Hellodds
+Traceback (most recent call last):
+  File "<pyshell#1>", line 1, in <module>
+    mystring = Hellodds
+NameError: name 'Hellodds' is not defined
+mystring = "Hello World"
+mystring
+'Hello World'
+mystring[5]
+' '
+mystring[4]
+'o'
+mystring[-1]
+'d'
+mystring = "abcdefghijklmnop"
+mystring
+'abcdefghijklmnop'
+mystring[:d]
+Traceback (most recent call last):
+  File "<pyshell#9>", line 1, in <module>
+    mystring[:d]
+NameError: name 'd' is not defined. Did you mean: 'id'?
+mystring[:3]
+'abc'
+mystring[3:]
+'defghijklmnop'
+mystring[3:]
